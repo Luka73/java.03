fdd120de503c929b68091cfacc724d156c50f1e8
Luana Fernandes Teixeira <p80357@csi.aneas.msft>
2019-02-26 19:25:42 -0300

    Conteudo da terceira aula

---
+++ src/TestaCaracteres.java
@@ -0,0 +1,22 @@
+
+public class TestaCaracteres {
+
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char) (valor + 1);
+		System.out.println(valor);
+		
+		String palavra = "Luana é a melhor professora do mundo!";
+		System.out.println(palavra);
+		
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+
+	}
+
+}
---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,21 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("Meu salário é: " + salario);
+		System.out.printf("Meu salário é: %.2f", salario);
+		
+		double divisao = 3.14/2;
+		System.out.println("\n" + divisao);
+		
+		int outraDivisao = 5/2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0/2;
+		System.out.println(novaTentativa);
+
+	}
+
+}
